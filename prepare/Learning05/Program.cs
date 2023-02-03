3c454e9ad1df1f1260d4b95ce872370f2f4c83d2
Aaron Webster <aaron.thomas.webster@gmail.com>
2023-02-02 21:20:19 -0700

    polymorphism prep

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Circle("green", 5));
+        shapes.Add(new Rectangle("blue",5, 10));
+        shapes.Add(new Square("Red",5));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine("The {0} shape has an area of {1}", color, area);
+        }
     }
 }
